6eebfd74a465b8558055370731554161ae0e58c3
Alfred.Ning <15667269283@163.com>
2023-12-24 11:29:05 +0800

    init

---
+++ test-hzero-repo/src/test/java/com/alfred/testhzerorepo/TestHzeroRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfred.testhzerorepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHzeroRepoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
